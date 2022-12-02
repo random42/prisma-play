c663a2eed9e1c4ae3e7237441bc06f1708d0a521
Roberto Sero <>
2022-12-02 16:27:14 +0000

    prisma update

--- src/client.ts
+++ src/client.ts
@@ -1,19 +1,25 @@
 import { PrismaClient } from '@prisma/client';
 
-type Extension = Parameters<PrismaClient['$extends']>[0];
+// type Extension = Parameters<PrismaClient['$extends']>[0];
 
 // const extension: Extension = ;
 
 export const db = new PrismaClient().$extends({
   model: {
     $allModels: {
-      async findPaginated<T = any>() {
+      async findPaginated<T = any>(args?) {
         const self = this as any;
         return {
           page: 1,
-          data: (await self.findMany()) as T[],
-          count: (await self.count()) as number,
+          data: (await self.findMany(args)) as T[],
+          count: (await self.count(args)) as number,
         };
+      },
+    },
+    user: {
+      async current() {
+        const self = this as any;
+        return self.findMany();
       },
     },
   },
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,10 @@
-// import { db } from './client';
+import { db } from './client';
 
 import { PrismaClient } from '@prisma/client';
 
 async function main() {
-  const db = new PrismaClient();
-  db.user.findPaginated();
-  console.log('Hello world!');
+  // const db = new PrismaClient();
+  console.log(await db.user.findPaginated());
 }
 
 main().catch(console.error);
